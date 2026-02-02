7dc297f916bc8eb945c91f370fd75ac004ac9a36
michael <michaelturner@MacBookAir.attlocal.net>
2026-02-02 07:52:53 -0500

    added a server to keep replit instance running

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("Bot is alive.");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
